4f15db4f5bfc931f3b3cf7caca77ef8fc2324658
Boyan Yin <38168034+BoyanYin@users.noreply.github.com>
2026-02-12 16:55:54 -0500

    Add conda installation for matplotlib and numba

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 pip install setuptools_scm
 pip install astropy==7.2
+conda install matplotlib
+conda install numba
 pip install git+https://github.com/chto/rail_base.git --no-build-isolation 
 pip install git+https://github.com/chto/tables_io.git
 pip install .
