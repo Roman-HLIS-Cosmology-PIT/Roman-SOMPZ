89a759764efd661782bfb406057af3864069c182
Boyan Yin <38168034+BoyanYin@users.noreply.github.com>
2026-02-12 16:59:41 -0500

    Add tqdm installation to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ pip install setuptools_scm
 pip install astropy==7.2
 conda install matplotlib
 conda install numba
+conda install tqdm
 pip install git+https://github.com/chto/rail_base.git --no-build-isolation 
 pip install git+https://github.com/chto/tables_io.git
 pip install .
