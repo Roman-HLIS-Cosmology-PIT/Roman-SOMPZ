e97c47ef030ad727dc79b17b6c99c8ce968d89b8
Boyan Yin <38168034+BoyanYin@users.noreply.github.com>
2026-02-12 17:10:45 -0500

    Add camb installation to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ conda install matplotlib
 conda install numba
 conda install tqdm
 conda install pyarrow
+conda install camb
 pip install git+https://github.com/chto/rail_base.git --no-build-isolation 
 pip install git+https://github.com/chto/tables_io.git
 pip install .
